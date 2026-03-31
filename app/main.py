aea502f777be757dc15667994c7e0fee9bdeccee
Mapurunga <luizotaviomv07@gmail.com>
2026-03-31 14:08:25 -0300

    add rota de i.a

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from app.routes.ia_router import router
 
 app= FastAPI()
+app.include_router(router)
 
 @app.get("/")
 def red_road():
-    return {"mensagem: API funcionando"}
+    return {"mensagem": "API funcionando"}
