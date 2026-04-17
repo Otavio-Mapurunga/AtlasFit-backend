7d3e0884bf2acc0499ad00f25ed625ed67d7bca2
Mapurunga <luizotaviomv07@gmail.com>
2026-04-17 11:07:24 -0300

    sam, resolve isso

--- app/repositories/exercicios_repository.py
+++ app/repositories/exercicios_repository.py
@@ -4,11 +4,20 @@ def buscar_exercicios():
     """Busca todos os exercícios cadastrados no Supabase."""
     response = supabase.table("exercicios").select("name","id","primaryMuscles").execute()
     return response.data
-def salvar_treino(usuario_id: str, _treino: dict | None = None):
+def salvar_treino(id_aluno: str, nome_treino: dict | None = None):
     response=supabase.table("treinos").insert({
-        "id_aluno":usuario_id,
-        "nome_treino":"treino IA",
+        "id_aluno":id_aluno,
+        "nome_treino":nome_treino,
         "objetivo":"gerado automaticamente",
     }).execute()
     #sempre usar () para executar um requerimento ou ação, sem isso não funciona
-    return response.data
+    return response.data[0]["id"]
+
+def salvar_exercicio_no_treino(id_treino:str,id_exercicio:str,series:int,repeticoes:str):
+    supabase.table("treino_exercicios").insert({
+        "id_treino":id_treino,
+        "id_exercicio":id_exercicio,
+        "series":series,
+        "repeticoes":repeticoes
+    }).execute()
+    return 
--- app/routes/ia_router.py
+++ app/routes/ia_router.py
@@ -4,7 +4,7 @@ import json
 from fastapi import HTTPException
 from app.services.gemini_service import gerar_treino
 from app.schemas.treino_schemas import AlunoRequest,TreinoResponse
-from app.repositories.exercicios_repository import buscar_exercicios, salvar_treino
+from app.repositories.exercicios_repository import buscar_exercicios, salvar_treino,salvar_exercicio_no_treino
 
 #router vai servir para facilitar a troca de url do site. prefixo /ia serve para deixar a url mais padronizada e limpa quando for utilizado algo de ia
 router = APIRouter(prefix="/ia")
@@ -19,7 +19,8 @@ def testar_ai():
 @router.post("/generate-workout",response_model=TreinoResponse)
 def treino_gerado(aluno: AlunoRequest):
     exercicios = buscar_exercicios()
-    lista_exercicios="\n".join([f"- {e['name']}" for e in exercicios])
+    lista_exercicios="\n".join([f"- {e['name']}" for e in exercicios])#type: ignore
+    mapa_exercicio={e['name']: e['id'] for e in exercicios}#type: ignore
     prompt= f""" 
     Responda com um JSON valido
     sem explicações
@@ -49,7 +50,7 @@ def treino_gerado(aluno: AlunoRequest):
     """
 
     resposta=gerar_treino(prompt)
-    resposta=resposta.replace("```json","").replace("```","")
+    resposta=resposta.replace("```json","").replace("```","")# type: ignore
     resposta=resposta.strip()
     print(f"Resposta da i.a:",resposta)
     try:
@@ -57,8 +58,24 @@ def treino_gerado(aluno: AlunoRequest):
     except json.JSONDecodeError:
        raise HTTPException(status_code=400, detail="A i.a retornou um JSON invalido")
     treino_validado=TreinoResponse(**dados_json)
-    usuario_id="522a1f07-9408-4f3f-b90c-783862846f3e"
-    salvar_treino(usuario_id, treino_validado.dict())
-    
+    id_aluno="522a1f07-9408-4f3f-b90c-783862846f3e"
+    for nome_treino,exercicios in treino_validado.treino.items():
+       id_treino = salvar_treino(id_aluno, nome_treino)
+
+       for ex in exercicios:
+          id_execercicio=mapa_exercicio.get(ex.exercicio)
+         
+          if not id_execercicio:
+             raise HTTPException(
+               status_code=400,
+               detail=f"Não foi possivel encontrar o id do exercicio {ex.exercicio}"
+             )
+          salvar_exercicio_no_treino(
+             id_treino=id_treino, # type: ignore
+             id_exercicio=id_execercicio, # type: ignore
+             series=ex.series,
+             repeticoes=ex.reps
+          )
+
     return treino_validado
     
