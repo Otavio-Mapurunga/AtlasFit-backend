3fe1b104b391c312537da919736a554335c9fada
pabloAfk <arturpablomm@gmail.com>
2026-04-22 10:43:51 -0300

    Update projetoacademia.py

--- app/projetoacademia.py
+++ app/projetoacademia.py
@@ -2,4 +2,8 @@ def calcular_progressao(carga_atual, nivel):
 taxas = {
         "iniciante": 0.05,
 "intermediario": 0.025,
-        "avanca
+        "avancado": 0.01
+    }
+    if nivel not in taxas:
+        raise ValueError("Nível inválido")
+    return round(carga_atual * (1 + taxas[nivel]), 2)
